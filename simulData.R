b9a13fc157993f71a776d4f1f5cf7bc753d14735
Michel <kintelalpz@gmail.com>
2023-04-24 12:46:21 +0200

    Implémentaion finale du modèle d'inductance et graphes

--- simulData.R
+++ simulData.R
@@ -1,3 +1,6 @@
+
+library(ggplot2)
+
 # f<-function(x) 2*x^2 + 10*x + 1
 # g<-function(x) 2.8*x^2 + 5*x + 1
 # h<-function(x) 3.6*x^2 + 1*x + 1
@@ -10,16 +13,40 @@
 # curve(f1,0,100,col="blue")
 # curve(f2,0,100,col="red", add=TRUE)
 
+#VAL=data[data$N == 60 & data$echs==1,]
+#plot(VAL$F, VAL$L, type="l")
+#VAL=data[data$N == 80 & data$echs==1,]
+#lines(VAL$F, VAL$L, col="red")
+
 #---------------------------------------------------------------------------
+plotFunction<-function(func, xm, xM, main, xlab, ylab){
+  
+  fonction<-parse(text=func)
+  fonction<-eval(fonction)
+  x <- seq(xm, xM, by=1)
+  val <- data.frame(x = x, y = fonction(x))
+  p<-(
+    ggplot(data = val, aes(x = x, y = y))+#, color="red")) + 
+      geom_line(color="red") + 
+      scale_x_continuous(name = xlab) +
+      scale_y_continuous(name = ylab) +
+      ggtitle(main)
+  )
+  
+}
 
-model<-function(F, N) {(0.01*N+1) * F^2 + (0.001*N-10) *F  + (0.02*N + 1)}
+facteur<-3.5
+modL<-function(F, N) { 
+  y<-(0.008*N+1) * F^2 + (0.001*N-10) *F  + (0.02*N + 1)
+  return(y/facteur)  
+}
 
-f<-function(F){ model(F, 10)}  # N=60
-g<-function(F){ model(F, 80)}  # N=80
-h<-function(F){ model(F, 100)}  # N=100
-k<-function(F){ model(F, 120)}  # N=120
+f<-function(F){ modL(F, 60)}  # N=60
+g<-function(F){ modL(F, 80)}  # N=80
+h<-function(F){ modL(F, 100)}  # N=100
+k<-function(F){ modL(F, 120)}  # N=120
 
-curve(f,10,100,col="blue")
+curve(f,10,100,col="blue", xlab = "F")
 curve(g,10,100,col="red", add=TRUE)
 curve(h,10,100,col="green", add=TRUE)
 curve(k,10,100,col="orange", add=TRUE)
@@ -29,18 +56,19 @@ frequencies<-c(10,20,28.5,40,50,66.6,100)
 nTypes<-c(60, 80, 100, 120)
 echs<-c()
 
+
+# Génération des inductances
+
 for(k in 1:3){
   
-  for(i in frequencies){
+  for(i in nTypes){
     
-    for(j in nTypes){
-      
-      L<-model(i,j)
+      L<-modL(c(10,20,28.5,40,50,66.6,100),i)
       inductances<-append(inductances, L)
       echs<-append(echs, k)
     }
     
-  }
+  
   
 }
 
@@ -50,7 +78,46 @@ N<-c(rep(nTypes[1],7), rep(nTypes[2],7), rep(nTypes[3],7),rep(nTypes[4],7))
 
 data<-data.frame(echs, N, F, inductancesBruit, inductancesBruit)
 colnames(data)[4] = "L"
+colnames(data)[5] = "R"
 write.csv(data,"./data/data.csv", row.names = FALSE)
 
+
+# Réprésentation des inductances
+colr<-c("blue", "red", "green", "orange")
+c<-0
+compteur<-1
+f<-seq(10, 100, by=1)
+VAL=data[data$N == 60 & data$echs==1,]
+plot(VAL$F, VAL$L)
+
+for(e in unique(echs)){
+  for (n in nTypes){
+    
+    
+    VAL=data[data$N == n & data$echs==e,] 
+    mod <- lm(VAL$L~VAL$F+I(VAL$F^2), data=VAL)
+   
+    
+    print(summary(mod))
+    
+    titre<-paste("Inductance / Fréquence - N=", as.character(n), "pour éch:", as.character(e))
+    Lest<-function(F){mod$coefficients[1]  + mod$coefficients[2] * F +   mod$coefficients[3] * F^2}
+    # p<-(ggplot(VAL) + geom_point(aes(x = F, y = L), colour = "#4271AE")  + stat_function(fun = Lest, color = "red") +  ggtitle(titre) 
+    #   
+    # )
+    # #png("L1.png")
+    # print(p)
+    # #dev.off()
+    print(paste("COMPTEUR:", compteur))
+    compteur<-compteur + 1
+    
+    
+    points(VAL$F, VAL$L)
+    lines(f, Lest(f), col=colr[c], add=TRUE)
+    c<-c + 1
+    
+  }
+
+}
 
 
--- tabMod.R
+++ tabMod.R
@@ -14,86 +14,6 @@ getMeasures<-function(file, s=";", d=","){
   
 }
 
-# 
-# MODSLN<-list()
-# MODSRN<-list()
-# 
-# MODSLF<-list()
-# MODSRF<-list()
-# 
-# frequencies<-unique(TAB$F)     # Listes des fréquences 
-# echs<-unique(TAB$ech)
-# nTypes<-unique(TAB$N)
-# 
-# 
-# # pour F=f0 
-# # 1- L=f(N)
-# # 2- R=f(N)
-# 
-# 
-# #pour N=no 
-# # 1- L=g(F)
-# # 2- R=g(F)
-# 
-# 
-# #---------------------    Courbes paramétrées par F      ------------------------------
-# 
-# # Inductance vs N paramétrée en F
-# j<-1
-#     
-# for(i in frequencies){
-#      
-#     
-#      VAL=TAB[TAB$F == i,]
-#      modL <- lm(VAL$L~VAL$N+I(VAL$N^2), data=VAL)
-#      MODSLN[[j]]<-modL
-#      j<-j+1
-#   
-# }
-# 
-# # Résistance vs N paramétrée en F
-# 
-# j<-1
-# 
-# for(i in frequencies){
-#   
-#   
-#   VAL=TAB[TAB$F == i,]
-#   modR <- lm(VAL$R~VAL$N+I(VAL$N^2), data=VAL)
-#   MODSRN[[j]]<-modR
-#   j<-j+1
-#   
-# }
-# 
-# # ---------------    Courbes paramétrées par N   -------------------------------
-# # Inductance vs F paramétrée en N
-# 
-# j<-1
-# 
-# for(i in nTypes){
-#   
-#   
-#   VAL=TAB[TAB$N == i,]
-#   modL <- lm(VAL$L~VAL$F+I(VAL$F^2), data=VAL)
-#   MODSLF[[j]]<-modL
-#   j<-j+1
-#   
-# }
-# 
-# 
-# # Résistance vs F paramétrée en N
-# 
-# j<-1
-# 
-# for(i in nTypes){
-#   
-#   
-#   VAL=TAB[TAB$N == i,]
-#   modR <- lm(VAL$R~VAL$F+I(VAL$F^2), data=VAL)
-#   MODSRF[[j]]<-modR
-#   j<-j+1
-#   
-# }
 
 getModels<-function(TAB){
   
@@ -185,6 +105,55 @@ getModels<-function(TAB){
   
 }
 
+estimateurs<-function(models, groupe){  # ex appel: M<-getModels(TAB)
+                                        #           estimateurs(M, "LN")
+  
+  EST<-list()
+  
+  # groupes: LN, RN, LF, RF
+  # models: liste de tous les models d'après le jeu de données traité
+  
+  if(groupe == "LN"){
+    
+    gr<-"MODSLN"
+  }
+  
+  if(groupe == "RN"){
+    
+    gr<-"MODSRN"
+  }
+  
+  if(groupe == "LF"){
+    
+    gr<-"MODSLF"
+  }
+  
+  
+  if(groupe == "RF"){
+    
+    gr<-"MODSRF"
+    
+  }
+  
+  # MDS<-parse(text=gr)
+  # MDS<-eval(MDS)
+  
+  i<-1
+  for(m in models[[gr]]){
+    
+    est<-paste(as.character(m$coefficients[1]), "+", as.character(m$coefficients[2]), "*x +", as.character(m$coefficients[3]),  "*x^2")
+    EST[[i]]<-est
+    i<-i+1
+    
+  }
+  
+  return(EST)
+  
+  #return(models[[gr]])
+  
+  
+}
+
 
 
 # --------------  TESTS DIVERS  ------------------------------------------------------
@@ -245,60 +214,8 @@ estimByF<-function(i, F, groupe){ # Exemple d'appel: estimateur2(1, 80, "MODSR")
   
 }
 
-estimateurs<-function(models, groupe){
-  
-  EST<-list()
-  
-  # groupes: LN, RN, LF, RF
-  # models: liste de tous les models d'après le jeu de données traité
-  
-  if(groupe == "LN"){
-    
-    gr<-"MODSLN"
-  }
-  
-  if(groupe == "RN"){
-    
-    gr<-"MODSRN"
-  }
-  
-  if(groupe == "LF"){
-    
-    gr<-"MODSLF"
-  }
-  
-  
-  if(groupe == "RF"){
-    
-    gr<-"MODSRF"
-    
-  }
-  
-  # MDS<-parse(text=gr)
-  # MDS<-eval(MDS)
-  
-  i<-1
-   for(m in models[[gr]]){
-  
-  
-     est<-function(x){
-  
-       m$coefficients[1] +  m$coefficients[2]*x +  m$coefficients[3]*x^2
-  
-     }
-  
-     EST[[i]]<-est
-     i<-i+1
-  
-   }
 
-  return(EST)
-
-  #return(models[[gr]])
-  
-  
-}
-
+#---------------------------------------------------------------------------------------------------
 
 #  FONCTIONS DE GENERATION DE DONNEES
 
